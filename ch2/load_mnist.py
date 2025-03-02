e4d9c6257c0a3391988db857755e1cff443e7893
Doug Woodward <doug@fourthpin.com>
2025-03-01 21:50:57 -0800

    adds param init

--- ch2/load_mnist.py
+++ ch2/load_mnist.py
@@ -1,6 +1,8 @@
 from os import WEXITSTATUS
+from PIL.Image import init
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from jax import random
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import Tuple
@@ -36,6 +38,15 @@ def show_data_sample(data: tf.data.Dataset) -> None:
 def preprocess(img: np.array, label: int) -> Tuple[np.array, int]:
     return (tf.cast(img, tf.float32)/255.0), label
 
+
+def init_network_params(sizes, key=random.PRNGKey(40), scale=1e-2):
+
+    def random_layer_params(m, n, key, scale=1e-2):
+        w_key, b_key = random.split(key)
+        return scale * random.normal(b_key, (n,))
+    keys = random.split(key, len(sizes))
+    return [random_layer_params(m, n, k, scale) for m, n, k in zip(sizes[:-1], sizes[1:], keys)]
+    
 if __name__ == "__main__":
     HEIGHT, WIDTH, CHANNELS = 28,28,1
     NUM_PIXELS = HEIGHT * WIDTH * CHANNELS
@@ -45,5 +56,10 @@ if __name__ == "__main__":
     # show_data_sample(data_train)
     train_data = tfds.as_numpy(data_train.map(preprocess).batch(32).prefetch(1))
     test_data = tfds.as_numpy(data_test.map(preprocess).batch(32).prefetch(1))
+
+    LAYER_SIZES = [28*28, 512, 10]
+    PARAM_SCALE = 0.01
+    params = init_network_params(LAYER_SIZES, random.PRNGKey(40), scale=PARAM_SCALE)
+
 
 
