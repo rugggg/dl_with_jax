8ece32497a6dc91b2d934e3531870530de6c8489
Doug Woodward <doug@fourthpin.com>
2025-03-23 21:15:23 -0700

    add note for jit with static argnums as decorator

--- ch5/xla.py
+++ ch5/xla.py
@@ -17,12 +17,51 @@ selu_jit(x).block_until_ready()
 
 # or
 @jax.jit
-def selu(x, alpha=1.6732, scale=1.0507):
+def selu_j(x, alpha=1.6732, scale=1.0507):
     return scale * jnp.where(x>0, x, alpha * jnp.exp(x) - alpha)
 
 # warmup call 
-z = selu(x)
-selu(x).block_until_ready()
-
+z = selu_j(x)
+selu_j(x).block_until_ready()
 
 # jit gets like a 5x speedup here fwiw
+
+# jit also has a backend param to compile for a specific device, ie cpu/gpu/tpu
+
+selu_jit_cpu = jax.jit(selu, backend="cpu")
+selu_jit_gpu = jax.jit(selu, backend="gpu")
+
+# to properly benchmark, you should also place data onto the correct device!
+x_cpu = jax.device_put(x, jax.devices('cpu')[0])
+# x_gpu = jax.device_put(x, jax.devices('gpu')[0])
+
+# static arguments
+
+def dense_layer(x, w, b, activation_func):
+    return activation_func(x*w+b)
+
+x = jnp.array([1.0, 2.0, 3.0])
+w = jnp.ones((3,3))
+b = jnp.ones(3)
+
+try:
+    dense_layer_jit = jax.jit(dense_layer)
+    dense_layer_jit(x, w, b, selu) # will fail beacuse of the last arg being a function!
+except TypeError as e:
+    print(e)
+
+
+dense_layer_jit = jax.jit(dense_layer, static_argnums=3)
+print(dense_layer_jit(x, w, b, selu))
+
+# another note on JIT static parameters is that for each value of a static param, the function being jitted is recompiled!
+
+# if you want to do jit with static args, use functools partial
+from functools import partial
+@partial(jax.jit, static_argnums=0)
+def dist(order, x, y):
+    return jnp.power(jnp.sum(jnp.abs(x-y)**order), 1.0/order)
+
+
+
+
