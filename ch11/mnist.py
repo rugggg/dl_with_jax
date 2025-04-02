872ee8418eded7665439adf4b4b7ff5bdbd3ca1a
Doug Woodward <doug@fourthpin.com>
2025-04-01 20:23:43 -0700

    add example loss and update fns

--- ch11/mnist.py
+++ ch11/mnist.py
@@ -11,7 +11,6 @@ class MLP(nn.Module):
         x = nn.activation.swish(x)
         x = nn.Dense(features=10)(x)
         return x
-
 model = MLP()
 
 key1, key2 = random.split(random.PRNGKey(0))
@@ -20,6 +19,19 @@ params = model.init(key2, random_flattened_image)
 print(jax.tree_util.tree_map(lambda x: x.shape, params))
 
 model.apply(params, random_flattened_image)
+
+def loss(params, images, targets):
+    """CCE loss"""
+    logits = model.apply(params, images)
+    log_preds = logits - jax.nn.logsumexp(logits)
+    return -jnp.mean(targets*log_preds)
+
+@jax.jit
+def update(params, x, y, epoch_number):
+    loss_value, grads = jax.value_and_grad(loss)(params, x, y)
+    lr = INIT_LR * DECAY_RATE ** (epoch_number / DECAY_STEPS)
+    return jax.tree_util.tree_map(lambda p, g: p - lt * g, params, grads), loss_value
+
 
 
 
